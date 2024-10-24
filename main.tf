d84649b0f5e9e73c475003fe9309f5a2a13ed715
Italo Rafael <83381713+italorafaeltavares@users.noreply.github.com>
2024-10-24 14:52:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,3 @@ resource "null_resource" "path" {
     command = "pwd"
   }
 }
-
-resource "null_resource" "create_file" {
-  provisioner "local-exec" {
-    command = "echo 'This is a sample file created by Terraform.' > my_file.txt"
-  }
-}
-
-resource "null_resource" "cleanup_file" {
-  provisioner "local-exec" {
-    command = "rm -f my_file.txt"
-  }
-}
